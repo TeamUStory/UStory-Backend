337d96378c709aad39d6aa7c343055d96f7f553b
wambatcode <feyfey@gmail.com>
2024-05-21 16:40:26 +0900

    Setting: 프로젝트 초기 설정

---
+++ src/main/java/com/ustory/ustory_service/UStoryApplication.java
@@ -0,0 +1,13 @@
+package com.ustory.ustory_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UStoryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UStoryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ustory/ustory_service/UStoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ustory.ustory_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UStoryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
