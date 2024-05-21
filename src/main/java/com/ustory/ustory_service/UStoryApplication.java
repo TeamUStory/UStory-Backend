3de44c00506a903833eca70e10bc074eba3676b4
wambatcode <feyfey@gmail.com>
2024-05-21 16:42:35 +0900

    Setting: SecurityAutoConfig 제외설정

--- src/main/java/com/ustory/ustory_service/UStoryApplication.java
+++ src/main/java/com/ustory/ustory_service/UStoryApplication.java
@@ -2,8 +2,9 @@ package com.ustory.ustory_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class UStoryApplication {
 
     public static void main(String[] args) {
