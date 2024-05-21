351d7d49e9c0e076f45dd77f4bc5dbfcf2893747
wambatcode <feyfey@gmail.com>
2024-05-21 21:45:44 +0900

    Feat: Comment Entity 및 Repository 추가

--- src/main/java/com/elice/ustory/domain/comment/entity/Comment.java
+++ src/main/java/com/elice/ustory/domain/comment/entity/Comment.java
@@ -1,4 +1,38 @@
 package com.elice.ustory.domain.comment.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "comment")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Comment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String content;
+
+    @CreatedDate
+    @Column(name = "created_at", nullable = false, updatable = false, columnDefinition = "TIMESTAMP")
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(name = "updated_at", nullable = false, columnDefinition = "TIMESTAMP")
+    private LocalDateTime updatedAt;
+
+//    @ManyToOne(fetch = FetchType.LAZY) TODO: Page 엔티티 만들어질 때까지 대기
+//    @JoinColumn(name = "page_id")
+//    private Page page;
 }
--- src/main/java/com/elice/ustory/domain/comment/repository/CommentRepository.java
+++ src/main/java/com/elice/ustory/domain/comment/repository/CommentRepository.java
@@ -1,4 +1,9 @@
 package com.elice.ustory.domain.comment.repository;
 
-public interface CommentRepository {
+import com.elice.ustory.domain.comment.entity.Comment;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CommentRepository extends JpaRepository<Comment, Long> {
 }
