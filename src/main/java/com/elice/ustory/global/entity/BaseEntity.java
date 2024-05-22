9b3772398abd996ccdcc99216fa15d83c81dc363
wambatcode <feyfey@gmail.com>
2024-05-22 10:59:35 +0900

    Feat: 공통된 TimeStamp를 위한 BaseEntity 추상 클래스 생성

---
+++ src/main/java/com/elice/ustory/global/entity/BaseEntity.java
@@ -0,0 +1,25 @@
+package com.elice.ustory.global.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@EntityListeners(AuditingEntityListener.class)
+@Getter
+@MappedSuperclass
+public abstract class BaseEntity {
+
+    @CreatedDate
+    @Column(name = "created_at", nullable = false, updatable = false, columnDefinition = "TIMESTAMP")
+    private LocalDateTime created_at;
+
+    @LastModifiedDate
+    @Column(name = "updated_at", nullable = false, columnDefinition = "TIMESTAMP")
+    private LocalDateTime updated_at;
+}
