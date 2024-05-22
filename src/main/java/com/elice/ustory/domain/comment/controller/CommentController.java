a5a6f9b530e4b696db3207a3cf26b6ba3e124302
wambatcode <feyfey@gmail.com>
2024-05-22 16:21:14 +0900

    Feat: Comment CRUD RestController 완성

--- src/main/java/com/elice/ustory/domain/comment/controller/CommentController.java
+++ src/main/java/com/elice/ustory/domain/comment/controller/CommentController.java
@@ -1,8 +1,11 @@
 package com.elice.ustory.domain.comment.controller;
 
+import com.elice.ustory.domain.comment.entity.Comment;
 import com.elice.ustory.domain.comment.service.CommentService;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/comment")
@@ -11,5 +14,30 @@ public class CommentController {
 
     public CommentController(CommentService commentService) {
         this.commentService = commentService;
+    }
+
+    @GetMapping("/{id}")
+    public Optional<Comment> getComment(@PathVariable Long id) {
+        return commentService.getComment(id);
+    }
+
+    @GetMapping
+    public List<Comment> getComments() {
+        return commentService.getComments();
+    }
+
+    @PostMapping
+    public Comment addComment(@RequestBody Comment comment) {
+        return commentService.addComment(comment);
+    }
+
+    @PutMapping("/{id}")
+    public Comment updateComment(@PathVariable Long id, @RequestBody Comment comment) {
+        return commentService.updateComment(comment);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteComment(@PathVariable Long id) {
+        commentService.deleteComment(id);
     }
 }
