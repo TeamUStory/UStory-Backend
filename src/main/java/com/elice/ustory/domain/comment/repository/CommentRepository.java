d26585715af17320fb6191bbc95c0f16600a0d06
wambatcode <feyfey@gmail.com>
2024-05-22 15:49:11 +0900

    Feat: Comment Service 기본 CRUD 메서드 작성

--- src/main/java/com/elice/ustory/domain/comment/repository/CommentRepository.java
+++ src/main/java/com/elice/ustory/domain/comment/repository/CommentRepository.java
@@ -4,6 +4,9 @@ import com.elice.ustory.domain.comment.entity.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface CommentRepository extends JpaRepository<Comment, Long> {
+    Optional<Comment> findById(Long id);
 }
--- src/main/java/com/elice/ustory/domain/comment/service/CommentService.java
+++ src/main/java/com/elice/ustory/domain/comment/service/CommentService.java
@@ -1,8 +1,12 @@
 package com.elice.ustory.domain.comment.service;
 
+import com.elice.ustory.domain.comment.entity.Comment;
 import com.elice.ustory.domain.comment.repository.CommentRepository;
 import jakarta.transaction.Transactional;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 @Transactional
@@ -11,5 +15,25 @@ public class CommentService {
 
     public CommentService(CommentRepository commentRepository) {
         this.commentRepository = commentRepository;
+    }
+
+    public List<Comment> getComments() {
+        return commentRepository.findAll();
+    }
+
+    public Optional<Comment> getComment(Long id) {
+        return commentRepository.findById(id);
+    }
+
+    public Comment addComment(Comment comment) {
+        return commentRepository.save(comment);
+    }
+
+    public Comment updateComment(Comment comment) {
+        return commentRepository.save(comment);
+    }
+
+    public void deleteComment(Long id) {
+        commentRepository.deleteById(id);
     }
 }
