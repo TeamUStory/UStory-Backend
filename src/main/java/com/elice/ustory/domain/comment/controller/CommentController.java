81b010d2e92d38165c06544b0e0334055acc1f59
wambatcode <feyfey927@gmail.com>
2024-05-22 19:46:20 +0900

    Refactor: Comment Controller 메소드 ResponseEntity로 반환되게 리팩토링

--- src/main/java/com/elice/ustory/domain/comment/controller/CommentController.java
+++ src/main/java/com/elice/ustory/domain/comment/controller/CommentController.java
@@ -2,6 +2,7 @@ package com.elice.ustory.domain.comment.controller;
 
 import com.elice.ustory.domain.comment.entity.Comment;
 import com.elice.ustory.domain.comment.service.CommentService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -17,27 +18,30 @@ public class CommentController {
     }
 
     @GetMapping("/{id}")
-    public Optional<Comment> getComment(@PathVariable Long id) {
-        return commentService.getComment(id);
+    public ResponseEntity<Comment> getComment(@PathVariable Long id) {
+        Optional<Comment> comment = commentService.getComment(id);
+        return comment.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
     }
 
     @GetMapping
-    public List<Comment> getComments() {
-        return commentService.getComments();
+    public ResponseEntity<List<Comment>> getComments() {
+        List<Comment> comments = commentService.getComments();
+        return ResponseEntity.ok().body(comments);
     }
 
     @PostMapping
-    public Comment addComment(@RequestBody Comment comment) {
-        return commentService.addComment(comment);
+    public ResponseEntity<Comment> createComment(@RequestBody Comment comment) {
+        return ResponseEntity.ok().body(commentService.addComment(comment));
     }
 
-    @PutMapping("/{id}")
-    public Comment updateComment(@RequestBody Comment comment) {
-        return commentService.updateComment(comment);
+    @PutMapping
+    public ResponseEntity<Comment> updateComment(@RequestBody Comment comment) {
+        return ResponseEntity.ok().body(commentService.updateComment(comment));
     }
 
     @DeleteMapping("/{id}")
-    public void deleteComment(@PathVariable Long id) {
+    public ResponseEntity<Void> deleteComment(@PathVariable Long id) {
         commentService.deleteComment(id);
+        return ResponseEntity.noContent().build();
     }
 }
