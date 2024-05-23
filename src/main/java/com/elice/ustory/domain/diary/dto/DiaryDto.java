85fa7b8cdd5fb427acdac26ba6b9d2a81c4e883e
jaewon926 <jjojjo0926@gmail.com>
2024-05-24 02:38:56 +0900

    feat:(Diary) Diary description 추가 & columnDefinition 지정

--- src/main/java/com/elice/ustory/domain/diary/dto/DiaryDto.java
+++ src/main/java/com/elice/ustory/domain/diary/dto/DiaryDto.java
@@ -1,4 +1,30 @@
 package com.elice.ustory.domain.diary.dto;
 
+import com.elice.ustory.domain.diary.entity.Diary;
+import com.elice.ustory.domain.diary.entity.DiaryCategory;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
 public class DiaryDto {
+    private String name;
+
+    private String diaryImg;
+
+    private DiaryCategory diaryCategory;
+
+    private String description;
+
+    public DiaryDto(String name, String diaryImg, DiaryCategory diaryCategory, String description) {
+        this.name = name;
+        this.diaryImg = diaryImg;
+        this.diaryCategory = diaryCategory;
+        this.description = description;
+    }
+
+    public Diary toDiary() {
+        Diary diary = new Diary(name, diaryImg, diaryCategory, description);
+        return diary;
+    }
 }
