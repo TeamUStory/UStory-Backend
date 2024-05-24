7541d25fe1706f868396a6802b83f1d3c7f92c69
GiJung <101462387+GiJungPark@users.noreply.github.com>
2024-05-24 16:49:31 +0900

    refactor: Repository 애너테이션 명시

--- src/main/java/com/elice/ustory/domain/paper/repository/ImageRepository.java
+++ src/main/java/com/elice/ustory/domain/paper/repository/ImageRepository.java
@@ -2,6 +2,8 @@ package com.elice.ustory.domain.paper.repository;
 
 import com.elice.ustory.domain.paper.entity.Image;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ImageRepository extends JpaRepository<Image, Long> {
 }
