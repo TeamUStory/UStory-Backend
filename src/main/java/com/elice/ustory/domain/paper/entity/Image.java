b11ae034f73204f7eca6aff44422e617e150b0bd
GiJung <101462387+GiJungPark@users.noreply.github.com>
2024-05-25 16:45:46 +0900

    refactor: Image JPA 제약조건 명시 - FK PAPER

--- src/main/java/com/elice/ustory/domain/paper/entity/Image.java
+++ src/main/java/com/elice/ustory/domain/paper/entity/Image.java
@@ -2,6 +2,7 @@ package com.elice.ustory.domain.paper.entity;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.ForeignKey;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -23,7 +24,7 @@ public class Image {
     private Long id;
 
     @ManyToOne
-    @JoinColumn(name = "paper_id")
+    @JoinColumn(name = "paper_id", referencedColumnName = "id", foreignKey = @ForeignKey(name = "FK_PAPER_ID"))
     private Paper paper;
 
     @Column(name = "image_url", nullable = false, columnDefinition = "varchar(1000)")
