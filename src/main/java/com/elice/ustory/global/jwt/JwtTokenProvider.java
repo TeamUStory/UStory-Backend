d32a14fd1716f8e9cee338d739a065102f7a7a04
wambatcode <feyfey927@gmail.com>
2024-05-27 07:19:41 +0900

    Feat: JwtTokenProvider & SecurityConfig 초기 코드 작성

--- src/main/java/com/elice/ustory/global/jwt/JwtTokenProvider.java
+++ src/main/java/com/elice/ustory/global/jwt/JwtTokenProvider.java
@@ -1,4 +1,44 @@
 package com.elice.ustory.global.jwt;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import jakarta.annotation.PostConstruct;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.nio.charset.StandardCharsets;
+import java.security.Key;
+import java.util.Date;
+
+@Component
+@Slf4j
 public class JwtTokenProvider {
+    private final long REFRESHTOKEN_VALID_MILISECOND = 1000L * 60 * 60 * 24 * 7;
+
+    @Value("${key.salt}")
+    private String salt;
+
+    private Key secretKey;
+
+    @PostConstruct
+    protected void init() {
+        log.info("[init] JwtTokenProvider 내 secretKey 초기화 시작");
+        secretKey = Keys.hmacShaKeyFor(salt.getBytes(StandardCharsets.UTF_8));
+        log.info("[init] JwtTokenProvider 내 SecretKey 초기화 완료");
+    }
+
+    public String createRefreshToken(String name){
+        Claims claims = Jwts.claims().setSubject(name);
+        Date now = new Date();
+        log.info("[createRefreshToken] Refresh 토큰 생성 완료");
+        return Jwts.builder()
+                .setClaims(claims)
+                .setIssuedAt(now)
+                .setExpiration(new Date(System.currentTimeMillis() + REFRESHTOKEN_VALID_MILISECOND))
+                .signWith(secretKey, SignatureAlgorithm.HS256)
+                .compact();
+    }
 }
