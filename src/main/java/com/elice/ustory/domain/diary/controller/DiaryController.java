9cd5b094c6811ea4d03c9a83de3522eec7a8da89
jaewon926 <jjojjo0926@gmail.com>
2024-05-28 19:53:37 +0900

    feat:(Diary) Diary Controller & Service에서 UsersList 파라미터 추가

--- src/main/java/com/elice/ustory/domain/diary/controller/DiaryController.java
+++ src/main/java/com/elice/ustory/domain/diary/controller/DiaryController.java
@@ -21,8 +21,8 @@ public class DiaryController {
 
     @Operation(summary = "Create Diary API", description = "다이어리 생성 및 링크 테이블에 등록")
     @PostMapping("/diary")
-    public ResponseEntity<Diary> createDiary(@RequestBody DiaryDto diaryDto){
-        Diary diary = diaryService.createDiary(diaryDto.toDiary());
+    public ResponseEntity<Diary> createDiary(@RequestBody DiaryDto diaryDto) {
+        Diary diary = diaryService.createDiary(diaryDto.toDiary(), diaryDto.getUsers());
 
         // TODO: Diary와 userId List를 링크 테이블에 저장 로직 필요
 
@@ -33,8 +33,8 @@ public class DiaryController {
     @Operation(summary = "Get User's Diary By User API", description = "유저가 속한 다이어리 목록 불러오기")
     @GetMapping("/diary")
     public ResponseEntity<List<Diary>> getDiaries(@RequestParam("userId") Long userId
-                                                ,@RequestParam(name = "page", defaultValue = "1") int page
-                                                ,@RequestParam(name = "size", defaultValue = "10") int size) {
+            , @RequestParam(name = "page", defaultValue = "1") int page
+            , @RequestParam(name = "size", defaultValue = "10") int size) {
         // TODO: userId를 통해 user-diary 링크 테이블의 다이어리 목록을 확인
         // TODO: 동적 쿼리를 통해 리스트의 다이어리 아이디를 통해 다이어리 리스트 반환
 
@@ -44,8 +44,8 @@ public class DiaryController {
 
     @Operation(summary = "Update Diary", description = "다이어리 정보 변경")
     @PutMapping("/diary/{diaryId}")
-    public ResponseEntity<Diary> updateDiary(@PathVariable("diaryId") Long diaryId, @RequestBody DiaryDto diaryDto){
-        if(diaryId==null){
+    public ResponseEntity<Diary> updateDiary(@PathVariable("diaryId") Long diaryId, @RequestBody DiaryDto diaryDto) {
+        if (diaryId == null) {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
         }
         Diary diary = diaryService.updateDiary(diaryId, diaryDto.toDiary());
@@ -54,21 +54,21 @@ public class DiaryController {
 
     @Operation(summary = "Get Diary By DiaryId", description = "다이어리 상세 페이지 불러오기")
     @GetMapping("/diary/{diaryId}")
-    public ResponseEntity<Diary> getDiaryByID(@PathVariable("diaryId") Long diaryId){
-        if(diaryId==null){
+    public ResponseEntity<Diary> getDiaryByID(@PathVariable("diaryId") Long diaryId) {
+        if (diaryId == null) {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
         }
         Diary diary = diaryService.getDiaryById(diaryId);
-        if(diary==null){
+        if (diary == null) {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
         }
 
         return ResponseEntity.ok(diary);
     }
 
-    @Operation(summary = "Delete Diary",description = "다이어리 삭제")
+    @Operation(summary = "Delete Diary", description = "다이어리 삭제")
     @DeleteMapping("/diary/{diaryId}")
-    public ResponseEntity<Void> deleteDiary(@PathVariable("diaryId") Long diaryId){
+    public ResponseEntity<Void> deleteDiary(@PathVariable("diaryId") Long diaryId) {
         // 모든 사람들이 다이어리에서 나갔을 때 어떻게 처리할 지
         diaryService.deleteDiary(diaryId);
         return ResponseEntity.noContent().build();
--- src/main/java/com/elice/ustory/domain/diary/service/DiaryService.java
+++ src/main/java/com/elice/ustory/domain/diary/service/DiaryService.java
@@ -2,16 +2,23 @@ package com.elice.ustory.domain.diary.service;
 
 import com.elice.ustory.domain.diary.entity.Diary;
 import com.elice.ustory.domain.diary.repository.DiaryRepository;
+import com.elice.ustory.domain.user.entity.Users;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class DiaryService {
     private final DiaryRepository diaryRepository;
 
-    public Diary createDiary(Diary diary) {
-        return diaryRepository.save(diary);
+    public Diary createDiary(Diary diary, List<Users> users) {
+        Diary savedDiary = diaryRepository.save(diary);
+
+        // TODO : Diary_User Table Update
+
+        return savedDiary;
     }
 
     public Diary getDiaryById(Long id) {
@@ -19,9 +26,9 @@ public class DiaryService {
         return findDiary;
     }
 
-    public Diary updateDiary(Long id, Diary diary){
+    public Diary updateDiary(Long id, Diary diary) {
         Diary updateDiary = diaryRepository.findById(id).orElse(null);
-        if(updateDiary==null) return null;
+        if (updateDiary == null) return null;
 
         updateDiary.updateDiary(diary);
         return updateDiary;
