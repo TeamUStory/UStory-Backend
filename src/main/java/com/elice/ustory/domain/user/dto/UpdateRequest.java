e0196602303b40a74ea1e8db5080f39d2dba57dc
GyungA <gga612@naver.com>
2024-05-28 19:27:49 +0900

    Feat: 유저 기본 CRUD //TODO: jwt, Security, OAuth. 각종 예외처리. - findByNicknameContaining 함수는 진희님 요청으로 추가됨 - UserPaperLink의 잘못된 엔티티 import 수정

---
+++ src/main/java/com/elice/ustory/domain/user/dto/UpdateRequest.java
@@ -0,0 +1,16 @@
+package com.elice.ustory.domain.user.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UpdateRequest {
+    private Long userId; //TODO: 쿠키에서 가져오기
+    private String name;
+    private String nickname;
+    private String password;
+    private String profileImg;
+}
