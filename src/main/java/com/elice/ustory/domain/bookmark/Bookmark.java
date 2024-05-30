dff5180a3a37f435d573c85dceb09290cb4cc1df
GiJung <101462387+GiJungPark@users.noreply.github.com>
2024-05-31 01:13:56 +0900

    feat: User와 Paper를 연결하는 Bookmark 테이블 생성

---
+++ src/main/java/com/elice/ustory/domain/bookmark/Bookmark.java
@@ -0,0 +1,42 @@
+package com.elice.ustory.domain.bookmark;
+
+import com.elice.ustory.domain.paper.entity.Paper;
+import com.elice.ustory.domain.user.entity.Users;
+import com.elice.ustory.global.entity.BaseEntity;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@EntityListeners(AuditingEntityListener.class)
+@Table(name = "bookmark")
+public class Bookmark extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private Users user;
+
+    @ManyToOne
+    @JoinColumn(name = "paper_id", nullable = false)
+    private Paper paper;
+
+    public Bookmark(Users user, Paper paper) {
+        this.user = user;
+        this.paper = paper;
+    }
+}
