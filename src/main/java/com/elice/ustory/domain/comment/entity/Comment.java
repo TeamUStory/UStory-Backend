1a570077b7540b08245bf2540bee65e788a7392c
wambatcode <feyfey927@gmail.com>
2024-05-31 20:47:05 +0900

    Feat: Comment content 글자 수 제한

--- src/main/java/com/elice/ustory/domain/comment/entity/Comment.java
+++ src/main/java/com/elice/ustory/domain/comment/entity/Comment.java
@@ -16,7 +16,7 @@ public class Comment extends BaseEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false)
+    @Column(nullable = false, length = 200)
     private String content;
 
     @ManyToOne
