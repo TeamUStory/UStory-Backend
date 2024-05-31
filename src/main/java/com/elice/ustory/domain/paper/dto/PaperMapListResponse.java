65a300ef8fd51e64d861791bf777c9d68ae1097a
GiJung <101462387+GiJungPark@users.noreply.github.com>
2024-05-31 16:50:02 +0900

    feat: 요구사항에 맞는 Response Dto 작성

---
+++ src/main/java/com/elice/ustory/domain/paper/dto/PaperMapListResponse.java
@@ -0,0 +1,42 @@
+package com.elice.ustory.domain.paper.dto;
+
+import com.elice.ustory.domain.paper.entity.Paper;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Getter;
+
+@Getter
+public class PaperMapListResponse {
+
+    @Schema(description = "타이틀", example = "여기 놀러갔었어요")
+    private String title;
+
+    @Schema(description = "썸네일 URL", example = "https://~~~~")
+    private String thumbnailImageUrl;
+
+
+    @Schema(description = "상호명", example = "우규")
+    private String store;
+
+    @Schema(description = "X좌표", example = "37.5494")
+    private Double coordinateX;
+
+    @Schema(description = "Y좌표", example = "126.9169")
+    private Double coordinateY;
+
+
+    @Schema(description = "다이어리 색상", example = "#FDD0B1")
+    private String diaryColor;
+
+    @Schema(description = "다이어리 이미지 URL", example = "https://~~~~")
+    private String diaryImageUrl;
+
+    public PaperMapListResponse(Paper paper) {
+        this.title = paper.getTitle();
+        this.thumbnailImageUrl = paper.getThumbnailImageUrl();
+        this.store = paper.getAddress().getStore();
+        this.coordinateX = paper.getAddress().getCoordinateX();
+        this.coordinateY = paper.getAddress().getCoordinateY();
+        this.diaryColor = paper.getDiary().getColor().getHexCode();
+        this.diaryImageUrl = paper.getDiary().getImgUrl();
+    }
+}
