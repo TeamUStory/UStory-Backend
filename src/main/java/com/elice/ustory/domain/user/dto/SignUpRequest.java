48c1ed0a745baf39ebc96be29173705a95c528bc
GyungA <gga612@naver.com>
2024-05-31 15:06:36 +0900

    Style: 필드명 변경 (profileImg -> profileImgUrl)

--- src/main/java/com/elice/ustory/domain/user/dto/SignUpRequest.java
+++ src/main/java/com/elice/ustory/domain/user/dto/SignUpRequest.java
@@ -1,6 +1,5 @@
 package com.elice.ustory.domain.user.dto;
 
-import com.elice.ustory.domain.user.entity.Users;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -10,9 +9,8 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class SignUpRequest {
     private String email;
-    private Users.LoginType loginType;
     private String name;
     private String nickname;
     private String password;
-    private String profileImg;
+    private String profileImgUrl;
 }
