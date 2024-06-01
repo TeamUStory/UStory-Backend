75423636f0e571d7486362d6522486b47292e33a
wambatcode <feyfey927@gmail.com>
2024-06-01 21:42:26 +0900

    Feat: postHandler 및 afterCompletion 메소드 추가

--- src/main/java/com/elice/ustory/global/config/WebMvcConfig.java
+++ src/main/java/com/elice/ustory/global/config/WebMvcConfig.java
@@ -6,16 +6,18 @@
 //import lombok.RequiredArgsConstructor;
 //import org.springframework.context.annotation.Configuration;
 //import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-//import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 //
 //@Configuration
 //@RequiredArgsConstructor
-//public class WebMvcConfig extends WebMvcConfigurationSupport {
+//public class WebMvcConfig implements WebMvcConfigurer {
 //    private final JwtTokenProvider jwtTokenProvider;
 //    private final JwtUtil jwtUtil;
 //
-//    protected void addInterceptors(InterceptorRegistry registry) {
+//    @Override
+//    public void addInterceptors(InterceptorRegistry registry) {
 //        registry.addInterceptor(new JwtInterceptor(jwtTokenProvider, jwtUtil))
 //                .addPathPatterns("/**");
+//        //TODO: 로그인 관련 엔드포인트는 제외시켜야함
 //    }
 //}
