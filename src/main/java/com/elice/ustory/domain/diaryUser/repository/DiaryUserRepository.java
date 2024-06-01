7fd8be476310dfff4ba7b19341a85ee0391fe057
jaewon926 <jjojjo0926@gmail.com>
2024-06-01 20:33:18 +0900

    rename:(Diary) DiaryUserRepositoryCustom 이름 재정의

--- src/main/java/com/elice/ustory/domain/diaryUser/repository/DiaryUserRepository.java
+++ src/main/java/com/elice/ustory/domain/diaryUser/repository/DiaryUserRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface DiaryUserRepository extends JpaRepository<DiaryUser, DiaryUserId>, DiaryUserRepositoryCustom {
+public interface DiaryUserRepository extends JpaRepository<DiaryUser, DiaryUserId>, DiaryUserQueryDslRepository {
 }
