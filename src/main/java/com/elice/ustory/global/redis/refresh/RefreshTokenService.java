3ce3e51f287115ecb95256a091baac508f0ac85c
bellra-jin <parkjin931@gmail.com>
2024-06-03 19:27:54 +0900

    style : redis 주석 품

--- src/main/java/com/elice/ustory/global/redis/refresh/RefreshTokenService.java
+++ src/main/java/com/elice/ustory/global/redis/refresh/RefreshTokenService.java
@@ -7,16 +7,16 @@ import org.springframework.stereotype.Service;
 @Service
 @RequiredArgsConstructor
 public class RefreshTokenService {
-//    private final RefreshTokenRepository refreshTokenRepository;
-//
-//    @Transactional
-//    public void saveTokenInfo(Long userId, String refreshToken, String accessToken) {
-//        refreshTokenRepository.save(new RefreshToken(String.valueOf(userId), refreshToken, accessToken));
-//    }
-//
-//    @Transactional
-//    public void removeTokenInfo(String accessToken) {
-//        refreshTokenRepository.findByAccessToken(accessToken)
-//                .ifPresent(refreshTokenRepository::delete);
-//    }
+    private final RefreshTokenRepository refreshTokenRepository;
+
+    @Transactional
+    public void saveTokenInfo(Long userId, String refreshToken, String accessToken) {
+        refreshTokenRepository.save(new RefreshToken(String.valueOf(userId), refreshToken, accessToken));
+    }
+
+    @Transactional
+    public void removeTokenInfo(String accessToken) {
+        refreshTokenRepository.findByAccessToken(accessToken)
+                .ifPresent(refreshTokenRepository::delete);
+    }
 }
