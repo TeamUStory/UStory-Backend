55187fbb47debd3788fc77caee8cdf7c28ba4d18
bellra-jin <parkjin931@gmail.com>
2024-06-03 10:53:54 +0900

    rename : FriendQueryDslRepository으로 변경

--- src/main/java/com/elice/ustory/domain/friend/repository/FriendQueryDslRepository.java
+++ src/main/java/com/elice/ustory/domain/friend/repository/FriendQueryDslRepository.java
@@ -2,11 +2,10 @@ package com.elice.ustory.domain.friend.repository;
 
 import com.elice.ustory.domain.friend.dto.FriendRequestDTO;
 import com.elice.ustory.domain.friend.dto.UserFriendDTO;
-import com.elice.ustory.domain.friend.dto.UserListDTO;
 
 import java.util.List;
 
-public interface FriendRepositoryCustom {
+public interface FriendQueryDslRepository {
     List<UserFriendDTO> findFriends(Long userId, String nickname);
     List<FriendRequestDTO> findFriendRequests(Long userId);
 
