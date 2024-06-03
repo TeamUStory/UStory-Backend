b3fbb62c5117dcffce94788f458c14c5ae3fc0eb
GiJung <101462387+GiJungPark@users.noreply.github.com>
2024-06-04 06:28:42 +0900

    feat: Bookmark 페이지네이션 기능 구현

--- src/main/java/com/elice/ustory/domain/bookmark/BookmarkRepository.java
+++ src/main/java/com/elice/ustory/domain/bookmark/BookmarkRepository.java
@@ -3,6 +3,7 @@ package com.elice.ustory.domain.bookmark;
 import com.elice.ustory.domain.bookmark.entity.Bookmark;
 import com.elice.ustory.domain.bookmark.entity.BookmarkId;
 import com.elice.ustory.domain.paper.entity.Paper;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -22,5 +23,5 @@ public interface BookmarkRepository extends JpaRepository<Bookmark, BookmarkId> 
 
     /** userId에 해당하는 Bookmark 들의 Paper List 가져오기 */
     @Query("SELECT b.paper FROM Bookmark b WHERE b.user.id = :userId")
-    List<Paper> findPapersByUserId(@Param("userId") Long userId);
+    List<Paper> findPapersByUserId(@Param("userId") Long userId, Pageable pageable);
 }
--- src/main/java/com/elice/ustory/domain/bookmark/BookmarkService.java
+++ src/main/java/com/elice/ustory/domain/bookmark/BookmarkService.java
@@ -2,10 +2,13 @@ package com.elice.ustory.domain.bookmark;
 
 import com.elice.ustory.domain.bookmark.entity.Bookmark;
 import com.elice.ustory.domain.paper.entity.Paper;
+import com.elice.ustory.domain.paper.repository.PaperRepository;
 import com.elice.ustory.domain.user.entity.Users;
+import com.elice.ustory.domain.user.repository.UserRepository;
 import com.elice.ustory.global.exception.model.ConflictException;
 import com.elice.ustory.global.exception.model.NotFoundException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -14,13 +17,26 @@ import java.util.List;
 @RequiredArgsConstructor
 public class BookmarkService {
 
+    private static final String NOT_FOUND_PAPER_MESSAGE = "%d: 해당하는 페이퍼가 존재하지 않습니다.";
+    private static final String NOT_FOUND_USER_MESSAGE = "%d: 해당하는 사용자가 존재하지 않습니다.";
+    private static final String NOT_FOUND_BOOKMARK_MESSAGE = "해당하는 북마크가 존재하지 않습니다.";
+    private static final String CONFLICT_BOOKMARK_MESSAGE = "이미 북마크로 지정되어 있습니다.";
+
     private final BookmarkRepository bookmarkRepository;
+    private final UserRepository userRepository;
+    private final PaperRepository paperRepository;
 
     /** 북마크 저장 */
-    public Bookmark saveBookmark(Users user, Paper paper) {
+    public Bookmark saveBookmark(Long userId, Long paperId) {
+
+        Users user = userRepository.findById(userId)
+                .orElseThrow(() -> new NotFoundException(String.format(NOT_FOUND_USER_MESSAGE, userId)));
+
+        Paper paper = paperRepository.findById(paperId)
+                .orElseThrow(() -> new NotFoundException(String.format(NOT_FOUND_PAPER_MESSAGE, paperId)));
 
         if (isPaperBookmarkedByUser(user.getId(), paper.getId())) {
-            throw new ConflictException("이미 북마크로 저장되어 있습니다.");
+            throw new ConflictException(CONFLICT_BOOKMARK_MESSAGE);
         }
 
         Bookmark bookmark = new Bookmark(user, paper);
@@ -28,8 +44,11 @@ public class BookmarkService {
     }
 
     /** 북마크 체크한 모든 paper 불러오기 */
-    public List<Paper> getBookmarksByUser(Long userId) {
-        return bookmarkRepository.findPapersByUserId(userId);
+    public List<Paper> getBookmarksByUser(Long userId, int page, int size) {
+
+        PageRequest pageRequest = PageRequest.of(page - 1, size);
+
+        return bookmarkRepository.findPapersByUserId(userId, pageRequest);
     }
 
     /** 북마크 판별 메서드 */
@@ -40,7 +59,7 @@ public class BookmarkService {
     /** 북마크 삭제 메서드 */
     public void deleteBookmark(Long userId, Long paperId) {
         Bookmark bookmark = bookmarkRepository.findByUserIdAndPaperId(userId, paperId)
-                .orElseThrow(() -> new NotFoundException("해당하는 북마크가 존재하지 않습니다."));
+                .orElseThrow(() -> new NotFoundException(NOT_FOUND_BOOKMARK_MESSAGE));
 
         bookmarkRepository.delete(bookmark);
     }
