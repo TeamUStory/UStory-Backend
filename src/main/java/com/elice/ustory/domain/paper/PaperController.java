c4796fa11e1c015380ac43dceb60716207e63486
[Cloud2]최윤기 <cwanthigh@gmail.com>
2024-06-06 21:14:38 +0900

    fix: @Valid

--- src/main/java/com/elice/ustory/domain/paper/PaperController.java
+++ src/main/java/com/elice/ustory/domain/paper/PaperController.java
@@ -58,7 +58,7 @@ public class PaperController {
     @PutMapping("/{paperId}")
     public ResponseEntity<UpdatePaperResponse> update(@PathVariable Long paperId,
                                                       @JwtAuthorization Long userId,
-                                                      @RequestBody UpdatePaperRequest updatePaperRequest) {
+                                                      @Valid @RequestBody UpdatePaperRequest updatePaperRequest) {
 
         Paper paper = paperService.update(userId, paperId, updatePaperRequest);
 
