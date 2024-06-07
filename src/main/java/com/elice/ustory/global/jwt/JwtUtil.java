356ccdbf13473dec5ee68eca04dd207b11d82551
wambatcode <feyfey927@gmail.com>
2024-06-07 16:05:30 +0900

    Fix: JwtUtil 필요 없는 코드 삭제

--- src/main/java/com/elice/ustory/global/jwt/JwtUtil.java
+++ src/main/java/com/elice/ustory/global/jwt/JwtUtil.java
@@ -1,6 +1,5 @@
 package com.elice.ustory.global.jwt;
 
-import com.elice.ustory.global.exception.model.ForbiddenException;
 import com.elice.ustory.global.redis.refresh.RefreshToken;
 import com.elice.ustory.global.redis.refresh.RefreshTokenRepository;
 import com.elice.ustory.global.redis.refresh.RefreshTokenService;
@@ -11,8 +10,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
 
-import java.io.UnsupportedEncodingException;
-
 @RequiredArgsConstructor
 @Component
 @Slf4j
@@ -22,12 +19,12 @@ public class JwtUtil {
     private final RefreshTokenService refreshTokenService;
 
     public boolean refreshAuthentication(HttpServletRequest request, HttpServletResponse response){
-        String accessToken = getTokenFromRequest(request); // TODO: Redis RefreshToken에 맞게 수정 1
+        String accessToken = getTokenFromRequest(request);
         RefreshToken refreshToken = refreshTokenRepository.findByAccessToken(accessToken)
                 .orElseThrow();
 
-
         log.info("redis안의 토큰: {}", refreshToken.getRefreshToken());
+
         if (jwtTokenProvider.validateToken(refreshToken.getRefreshToken())) {
             log.info("[refreshToken] 기존 RefreshToken으로 AccessToken 재발급 && 새 RefreshToken 발급 시작");
             String newAccessToken = jwtTokenProvider.createAccessToken(Long.valueOf(refreshToken.getId()));
@@ -38,13 +35,14 @@ public class JwtUtil {
 
             log.info("[refreshToken] AccessToken이 재발급 되었습니다: {}", newAccessToken);
             log.info("[refreshToken] RefreshToken이 재발급 되었습니다: {}", newRefreshToken);
+
             response.addHeader("Authorization", "Bearer " + newAccessToken);
             return true;
         } else {
             log.warn("[refreshToken] RefreshToken이 만료 되었습니다.");
             return false;
         }
-    } // TODO: Redis RefreshToken에 맞게 수정 2
+    }
 
     public String getTokenFromRequest(HttpServletRequest request){
         String bearerToken = request.getHeader("Authorization");
@@ -54,21 +52,5 @@ public class JwtUtil {
         }
 
         return null;
-
-//        if (request.getCookies() != null) {
-//            Optional<Cookie> tokenCookie = Arrays.stream(request.getCookies())
-//                    .filter(
-//                            cookie -> cookie.getName().equals(tokenName)
-//                    ).findFirst();
-//
-//            if (tokenCookie.isPresent()) {
-//                String token = URLDecoder.decode(tokenCookie.get().getValue(), "UTF-8");
-//                if (token != null && token.startsWith("Bearer ")) {
-//                    return token.substring(7);
-//                }
-//                log.info(token);
-//            }
-//        }
-//        return null;
     }
 }
