9e6e81e505b9ba52945584b3fc9822e34dc99a23
jaewon926 <jjojjo0926@gmail.com>
2024-06-07 19:03:34 +0900

    feat:(Diary) Diary update 유저 리스트에 자기자신이 포함되도록 변경

--- src/main/java/com/elice/ustory/domain/diary/service/DiaryService.java
+++ src/main/java/com/elice/ustory/domain/diary/service/DiaryService.java
@@ -108,7 +108,7 @@ public class DiaryService {
                 // request를 보낸 유저까지 10명을 초과하는 경우
                 throw new ValidationException("다이어리 인원을 10명을 초과할 수 없습니다.");
 
-            }else if(usersByDiary.size()<userList.size()){
+            }else if(usersByDiary.size()<userList.size()-1){
                 // 존재하지 않는 유저 닉네임이 보내진 경우 && 개인 다이어리의 경우(에러 메세지에 대한 분리 및 고민이 필요)
                 throw new NotFoundException("해당하는 친구가 존재하지 않습니다.");
             }
