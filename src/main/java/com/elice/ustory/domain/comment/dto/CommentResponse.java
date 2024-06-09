e276fcc0d43dc591ff1ac17ae948835f420eb20f
wambatcode <feyfey927@gmail.com>
2024-06-09 20:30:10 +0900

    Fix: Comment Response DTO 수정

--- src/main/java/com/elice/ustory/domain/comment/dto/CommentResponse.java
+++ src/main/java/com/elice/ustory/domain/comment/dto/CommentResponse.java
@@ -16,6 +16,7 @@ public class CommentResponse {
     private Long userId;
     private String userNickname;
     private String profileImg;
+    private int isUpdatable;
 
     @JsonSerialize(using = LocalDateConverter.class)
     private LocalDate createdAt;
@@ -26,6 +27,7 @@ public class CommentResponse {
         this.userId = comment.getUser().getId();
         this.userNickname = comment.getUser().getNickname();
         this.profileImg = comment.getUser().getProfileImgUrl();
+        this.isUpdatable = comment.getIsUpdatable();
         this.createdAt = LocalDate.now();
     }
 }
