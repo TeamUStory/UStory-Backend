e42d4d46db6532541a7b3c711247e9d5a8c4ebac
GyungA <gga612@naver.com>
2024-06-10 02:30:13 +0900

    Docs: SignUpRequest 문서 구체화

--- src/main/java/com/elice/ustory/domain/user/dto/SignUpRequest.java
+++ src/main/java/com/elice/ustory/domain/user/dto/SignUpRequest.java
@@ -1,15 +1,42 @@
 package com.elice.ustory.domain.user.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
+
+import static io.swagger.v3.oas.annotations.media.Schema.RequiredMode.REQUIRED;
 
 @Data
 public class SignUpRequest {
+    @NotNull
+    @Schema(requiredMode = REQUIRED, example = "example@email.com")
     private String email; // 인증 여부: done
+
+    @NotNull
+    @Schema(requiredMode = REQUIRED, example = "피카츄")
     private String name; // 인증 여부: to do
+
+    @NotNull
+    @Schema(requiredMode = REQUIRED, example = "Nick")
     private String nickname; // 인증 여부: done
+
+    @NotNull
+    @Schema(requiredMode = REQUIRED, description = "형식 조건에 유의하세요.", example = "password!!11")
     private String password; // 인증 여부: to do
+
+    @NotNull
+    @Schema(requiredMode = REQUIRED, description = "첫번째 입력한 비밀번호와 같은 값을 입력해야 합니다.", example = "password!!11")
     private String passwordCheck;
+
+    @NotNull
+    @Schema(requiredMode = REQUIRED, description = "회원가입 시 기본값을 입력받습니다.", example = "../images/profile.png")
     private String profileImgUrl; // 가입 시 기본값 입력(프론트에서 전달)
+
+    @NotNull
+    @Schema(requiredMode = REQUIRED, description = "회원가입 시 기본값을 입력받습니다.", example = "자기소개를 작성하지 않았습니다.")
     private String profileDescription; // 가입 시 기본값 입력(프론트에서 전달)
+
+    @NotNull
+    @Schema(requiredMode = REQUIRED, description = "회원가입 시 기본값을 입력받습니다.", example = "../images/diary.png")
     private String diaryImgUrl; // 가입 시 기본값 입력(프론트에서 전달)
 }
