3e94fd6003667ab234199f51e30788e84ce7a190
GyungA <gga612@naver.com>
2024-06-10 15:30:52 +0900

    Fix: nickname varchar 10->30

--- src/main/java/com/elice/ustory/domain/user/entity/Users.java
+++ src/main/java/com/elice/ustory/domain/user/entity/Users.java
@@ -29,7 +29,7 @@ public class Users extends BaseEntity {
     @Column(name = "name", columnDefinition = "VARCHAR(10)")
     private String name;
 
-    @Column(unique = true, columnDefinition = "VARCHAR(10) UNIQUE")
+    @Column(unique = true, columnDefinition = "VARCHAR(30) UNIQUE")
     private String nickname;
     //TODO: 닉네임 제한 다시 걸기(sql injection 대비. 허용되지 않은 값은 제외하고 받아오도록.)
 
