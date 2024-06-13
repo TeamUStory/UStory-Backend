9e55474d50e961c77c743f056eb223dc4f45c029
GyungA <gga612@naver.com>
2024-06-13 13:47:02 +0900

    Feat: requestDto에 정규식 확인 로직 추가

--- src/main/java/com/elice/ustory/domain/user/dto/ChangePwdRequest.java
+++ src/main/java/com/elice/ustory/domain/user/dto/ChangePwdRequest.java
@@ -1,11 +1,14 @@
 package com.elice.ustory.domain.user.dto;
 
+import com.elice.ustory.domain.user.entity.RegexPatterns;
 import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
 import lombok.Getter;
 
 @Getter
 public class ChangePwdRequest {
     @NotEmpty(message = "비밀번호 입력란은 필수 입력값입니다.")
+    @Pattern(regexp = RegexPatterns.PASSWORD_REG, message = "비밀번호 형식이 올바르지 않습니다.")
     String password;
 
     @NotEmpty(message = "비밀번호 확인란은 필수 입력값입니다.")
--- src/main/java/com/elice/ustory/domain/user/dto/UpdateRequest.java
+++ src/main/java/com/elice/ustory/domain/user/dto/UpdateRequest.java
@@ -1,12 +1,17 @@
 package com.elice.ustory.domain.user.dto;
 
+import com.elice.ustory.domain.user.entity.RegexPatterns;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 @Data
 public class UpdateRequest {
     private String name;
     private String nickname;
+
+    @Pattern(regexp = RegexPatterns.PASSWORD_REG, message = "비밀번호 형식이 올바르지 않습니다.")
     private String password;
+
     private String profileImgUrl;
     private String profileDescription;
 }
