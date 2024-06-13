b8ed3b681711d13c4a639329edf66d6ab6090cb6
GiJung <101462387+GiJungPark@users.noreply.github.com>
2024-06-13 23:45:20 +0900

    feat: Notice API Responses 명시

--- src/main/java/com/elice/ustory/domain/notice/controller/NoticeController.java
+++ src/main/java/com/elice/ustory/domain/notice/controller/NoticeController.java
@@ -1,11 +1,16 @@
 package com.elice.ustory.domain.notice.controller;
 
 import com.elice.ustory.domain.notice.dto.NoticeResponse;
-import com.elice.ustory.domain.notice.entity.Notice;
+import com.elice.ustory.global.exception.dto.ErrorResponse;
 import com.elice.ustory.global.exception.model.ValidationException;
 import com.elice.ustory.global.jwt.JwtAuthorization;
 import com.elice.ustory.domain.notice.service.NoticeService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 
 import org.springframework.data.domain.PageRequest;
@@ -17,7 +22,7 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 
-@Tag(name = "notice", description = "Notice API")
+@Tag(name = "Notice API")
 @RestController
 @RequestMapping("/notices")
 public class NoticeController {
@@ -37,7 +42,13 @@ public class NoticeController {
      * @return 알림 목록
      */
     @Operation(summary = "Get All Notice API",
-            description = "사용자가 가진 모든 알림 조회, type: 친구, 기록, 코멘트 중 하나로 반납합니다. 페이퍼로 이동해야 할 상황을 고려해서 paperId를 함께 넘기며, 만약 null 값이라면 친구와 관련된 API라고 생각하시면 됩니다.")
+            description = "사용자가 가진 모든 알림 조회, type: 친구, 기록, 코멘트 중 하나로 반납합니다. 페이퍼로 이동해야 할 상황을 고려해서 paperId를 함께 넘기며, 만약 null 값이라면 친구와 관련된 API라고 생각하시면 됩니다. <br> 출력할 데이터가 존재하지 않는 경우 빈 리스트를 반환합니다.")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "Ok", content = @Content(mediaType = "application/json", array = @ArraySchema(schema = @Schema(implementation = NoticeResponse.class)))),
+            @ApiResponse(responseCode = "400", description = "Bad Request", content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorResponse.class))),
+            @ApiResponse(responseCode = "401", description = "Unauthorized", content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorResponse.class))),
+            @ApiResponse(responseCode = "500", description = "Internal Server Error", content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorResponse.class)))
+    })
     @GetMapping
     public ResponseEntity<List<NoticeResponse>> getAllNoticesByUserId(@JwtAuthorization Long userId,
                                                                       @RequestParam(name = "page", defaultValue = "1") int page,
@@ -46,7 +57,7 @@ public class NoticeController {
         if (page < 1) {
             throw new ValidationException("페이지는 1 이상이어야 합니다.");
         } else if (size < 1){
-            throw new ValidationException("사이즈는 1 이상이어야합니다.");
+            throw new ValidationException("사이즈는 1 이상이어야 합니다.");
         }
 
         Pageable pageable = PageRequest.of(page - 1, size);
@@ -63,6 +74,13 @@ public class NoticeController {
      * @return 요청 성공 여부
      */
     @Operation(summary = "Delete / Notice", description = "알림을 삭제합니다.")
+    @ApiResponses({
+            @ApiResponse(responseCode = "204", description = "No Content", content = @Content(mediaType = "application/json")),
+            @ApiResponse(responseCode = "400", description = "Bad Request", content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorResponse.class))),
+            @ApiResponse(responseCode = "401", description = "Unauthorized", content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorResponse.class))),
+            @ApiResponse(responseCode = "404", description = "Not Found", content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorResponse.class))),
+            @ApiResponse(responseCode = "500", description = "Internal Server Error", content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorResponse.class)))
+    })
     @DeleteMapping("/{noticeId}")
     public ResponseEntity<Void> deleteNotice(@JwtAuthorization Long userId, @PathVariable Long noticeId) {
         noticeService.deleteNoticeById(userId, noticeId);
