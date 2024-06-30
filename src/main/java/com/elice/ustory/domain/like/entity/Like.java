7c1e17f8ea43108810c31d40e3597a60d26be11b
GiJung <101462387+GiJungPark@users.noreply.github.com>
2024-06-30 16:07:57 +0900

    feat: 좋아요 Entity 작성

---
+++ src/main/java/com/elice/ustory/domain/like/entity/Like.java
@@ -0,0 +1,40 @@
+package com.elice.ustory.domain.like.entity;
+
+import com.elice.ustory.domain.paper.entity.Paper;
+import com.elice.ustory.domain.user.entity.Users;
+import com.elice.ustory.global.entity.BaseEntity;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.Id;
+import jakarta.persistence.IdClass;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@EntityListeners(AuditingEntityListener.class)
+@IdClass(LikeId.class)
+@Table(name = "like")
+public class Like extends BaseEntity {
+
+    @Id
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private Users user;
+
+    @Id
+    @ManyToOne
+    @JoinColumn(name = "paper_id", nullable = false)
+    private Paper paper;
+
+    public Like(Users user, Paper paper) {
+        this.user = user;
+        this.paper = paper;
+    }
+}
