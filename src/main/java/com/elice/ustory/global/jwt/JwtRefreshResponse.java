344dfc97cfab06e8759cb7a56fee282f6c452c48
wambatcode <feyfey927@gmail.com>
2024-07-12 14:14:43 +0900

    Fix: JwtRefreshResponse 내 RequiredArgs 어노테이션 제거

--- src/main/java/com/elice/ustory/global/jwt/JwtRefreshResponse.java
+++ src/main/java/com/elice/ustory/global/jwt/JwtRefreshResponse.java
@@ -1,12 +1,10 @@
 package com.elice.ustory.global.jwt;
 
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
 
-@RequiredArgsConstructor
 @Getter
 public class JwtRefreshResponse {
-    private String refreshAccessToken;
+    private final String refreshAccessToken;
 
     public JwtRefreshResponse(String refreshAccessToken) {
         this.refreshAccessToken = refreshAccessToken;
